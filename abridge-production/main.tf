0089d2f10a1d34b9821e445aabe675e6bc6e3c89
jamesmaa <jamesmaa17@gmail.com>
2025-06-05 11:42:10 -0700

    Format

--- abridge-production/main.tf
+++ abridge-production/main.tf
@@ -4,30 +4,30 @@ provider "google" {
 }
 
 module "vpc_with_gke" {
-    source     = "./.."
-    project_id = var.project_id
+  source     = "./.."
+  project_id = var.project_id
 
-    # Networking variables
-    vpc_name   = "abridge-production-vpc"
-    region     = var.region
+  # Networking variables
+  vpc_name = "abridge-production-vpc"
+  region   = var.region
 
-    # GKE variables
-    gke_subnet_node_ip_cidr_range     = "10.0.0.0/14"
-    gke_subnet_pods_ip_cidr_range     = "10.4.0.0/14"
-    gke_subnet_services_ip_cidr_range = "10.8.0.0/18"
-    gke_cluster_name                   = "abridge-production-gke-cluster"
-    deletion_protection                = true
-    node_pools = [
-        {
-            name                 = "default-pool"
-            machine_type         = "e2-standard-4"
-            total_min_node_count = 1
-            total_max_node_count = 10
-            preemptible          = false
-            labels = {
-                "environment" = "production"
-                "team"        = "platform"
-            }
-        }
-    ]
+  # GKE variables
+  gke_subnet_node_ip_cidr_range     = "10.0.0.0/14"
+  gke_subnet_pods_ip_cidr_range     = "10.4.0.0/14"
+  gke_subnet_services_ip_cidr_range = "10.8.0.0/18"
+  gke_cluster_name                  = "abridge-production-gke-cluster"
+  deletion_protection               = true
+  node_pools = [
+    {
+      name                 = "default-pool"
+      machine_type         = "e2-standard-4"
+      total_min_node_count = 1
+      total_max_node_count = 10
+      preemptible          = false
+      labels = {
+        "environment" = "production"
+        "team"        = "platform"
+      }
+    }
+  ]
 }
--- abridge-staging/main.tf
+++ abridge-staging/main.tf
@@ -4,26 +4,26 @@ provider "google" {
 }
 
 module "vpc_with_gke" {
-    source     = "./.."
-    project_id = var.project_id
+  source     = "./.."
+  project_id = var.project_id
 
-    # Networking variables
-    vpc_name   = "abridge-staging-vpc"
-    region     = var.region
+  # Networking variables
+  vpc_name = "abridge-staging-vpc"
+  region   = var.region
 
-    # GKE variables
-    gke_subnet_node_ip_cidr_range     = "10.0.0.0/14"
-    gke_subnet_pods_ip_cidr_range     = "10.4.0.0/14"
-    gke_subnet_services_ip_cidr_range = "10.8.0.0/18"
-    gke_cluster_name                   = "abridge-staging-gke-cluster"
-    deletion_protection                = false
-    node_pools = [
-        {
-            name                 = "default-pool"
-            machine_type         = "e2-small"
-            total_min_node_count = 1
-            total_max_node_count = 3
-            preemptible          = false
-        }
-    ]
+  # GKE variables
+  gke_subnet_node_ip_cidr_range     = "10.0.0.0/14"
+  gke_subnet_pods_ip_cidr_range     = "10.4.0.0/14"
+  gke_subnet_services_ip_cidr_range = "10.8.0.0/18"
+  gke_cluster_name                  = "abridge-staging-gke-cluster"
+  deletion_protection               = false
+  node_pools = [
+    {
+      name                 = "default-pool"
+      machine_type         = "e2-small"
+      total_min_node_count = 1
+      total_max_node_count = 3
+      preemptible          = false
+    }
+  ]
 }
