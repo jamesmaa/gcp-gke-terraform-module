3f6986b9f0c7c773348702f5e3ba634886448774
jamesmaa <jamesmaa17@gmail.com>
2025-06-05 11:41:30 -0700

    Remove unused random

--- abridge-production/versions.tf
+++ abridge-production/versions.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4"
-    }
   }
 }
 
--- abridge-staging/versions.tf
+++ abridge-staging/versions.tf
@@ -6,18 +6,14 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4"
-    }
   }
 }
 
 
 # REPLACE ME with your own backend configuration
-# terraform {
-#   backend "gcs" {
-#     bucket = "jmaa-abridge-staging-demo"
-#     prefix = "terraform/state"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "jmaa-abridge-staging-demo"
+    prefix = "terraform/state"
+  }
+}
